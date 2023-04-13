5e4d40ee747a78bd84de402263b1436a8d87c0e0
larry2357 <larryj20q@gmail.com>
2023-04-13 07:47:08 -0700

    update

---
+++ business-loan-client/components/footer.js
@@ -0,0 +1,15 @@
+import styles from '../styles/Home.module.css'
+
+export default function Footer() {
+    return(
+        <footer className={styles.footer}>
+        <a
+          href="https://www.linkedin.com/in/yevhenii-molchanov/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Submitted by Yevhenii
+        </a>
+      </footer>
+    );
+}
